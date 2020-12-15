61eed25598afde8b9531ea1bc20a95b568f0fb60
jennyzhou12 <73356598+jennyzhou12@users.noreply.github.com>
2020-12-15 00:23:23 -0500

    interesting small project
    Those are some interesting small program created by me.
    amorpmclock.py- enter original hour and time passed, it will tell you the new time

---
+++ amorpmclock.py
@@ -0,0 +1,12 @@
+x=int(input("Enter hour:"))
+s=input("am or pm?")
+y=int(input("how many hours more?"))
+if s=="am":
+    newhour=x
+elif s=="pm":
+    newhour=x+12
+
+if((newhour+y)//12)%2==1:
+    print((newhour+y)%12,"pm")
+else:
+    print((newhour+y)%12,"am")

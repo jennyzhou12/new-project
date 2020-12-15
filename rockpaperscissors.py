0f52e6332ab41222770c4de74131ab0c56b748aa
jennyzhou12 <73356598+jennyzhou12@users.noreply.github.com>
2020-12-15 00:34:35 -0500

    Play rock paper scissors 5 times with computer

---
+++ rockpaperscissors.py
@@ -0,0 +1,59 @@
+# import random
+# a=0
+# for i in range(5):
+#     x=input("rock,paper or scissors")
+#     y=random.randrange(1,4)
+#     if y==1:
+#         if x=="paper":
+#             a-=1
+#         elif x=="scissors":
+#             a+=1
+#         print("rock")
+#     if y==2:
+#         if x=="rock":
+#             a+=1
+#         elif x=="scissors":
+#             a-=1
+#         print("paper")
+#     if y==3:
+#         if x=="paper":
+#             a+=1
+#         if x=="rock":
+#             a-=1
+#         print("scissors")
+# if a>0:
+#     print("computer wins")
+# elif a==0:
+#     print("tie")
+# else:
+#     print("user wins")
+
+
+import random
+a=0
+for i in range(5):
+    x=input("rock,paper or scissors")
+    if x=="rock":
+        x=1
+    if x=="scissors":
+        x=2
+    if x=="paper":
+        x=3
+    y=random.randrange(1,4)
+    if y==1:
+        print("rock")
+    if y==2:
+        print("paper")
+    if y==3:
+        print("scissors")
+    a=0
+    if x<y:
+        a=a+1
+    if x>y:
+        a=a-1
+if a>0:
+    print("computer wins")
+elif a==0:
+    print("tie")
+else:
+    print("user wins")
